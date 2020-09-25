78bedf18f90ca71451ae8f49c3819a30560f00b8
liujingyuan <906514731@qq.com>
2020-09-25 16:14:47 +0800

    add flex

--- src/flexTest.vue
+++ src/flexTest.vue
@@ -1,13 +1,52 @@
 <template>
-<h1>flex</h1>
+  <div>
+    <div class="home">
+      <button style="background-color: red;width:100%"></button>
+      <button style="background-color: yellow;width:100%"></button>
+      <button style="background-color: oldlace;width:100%"></button>
+      <button style="background-color: cadetblue;width:100%"></button>
+      <button style="background-color:blue;width:100%"></button>
+    </div>
+
+    <div class="secode">
+      <div class="thild">
+        <span style="background-color: #42b983;height: 50%; width: 100%">1</span>
+        <span style="background-color: firebrick;height: 50%;  width: 100%">2</span>
+      </div>
+      <span style="background-color: fuchsia;height: 200px; width: 50%">3</span>
+    </div>
+
+  </div>
 </template>
 
 <script>
 export default {
-name: "flexTest"
+  name: "flexTest"
 }
 </script>
 
 <style scoped>
+
+.home {
+  justify-content: space-around;
+  display: flex;
+  height: 200px;
+}
+
+.secode{
+  justify-content: space-around;
+  flex-direction: row;
+  display: flex;
+  height: 200px;
+}
+
+.thild{
+  justify-content: space-around;
+  width: 50%;
+  flex-direction: column;
+  display: flex;
+  height: 200px;
+}
+
 
 </style>
